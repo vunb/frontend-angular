0029792e30db67fbd472bd8e37dbcb877dc4619b
Vunb <nhubaovu@gmail.com>
2019-04-21 11:22:56 +0700

    refactor, config prod environtment

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Todo } from './../models/todo';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +13,20 @@ export class TodoService {
   constructor(
     public http: HttpClient
   ) {
-    this.apiUrl = 'https://node-todos.herokuapp.com/api/todos';
+    this.apiUrl = environment.API_URL + 'todo';
   }
 
   /**
    * Lấy danh sách các tasks
    */
   getTodoList() {
-    return this.http.get<Array<Todo>>(this.apiUrl);
+    const header = {
+      headers: new HttpHeaders()
+        .set('Authorization',  'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NTYzODYwMDEsImlkIjozLCJ1c2VybmFtZSI6InZ1bmIyIn0'
+        + '.rod-sbsHwoQC3C7ur78YS690d433L8805Y6p7z2_Pog')
+    };
+
+    return this.http.get<Array<Todo>>(this.apiUrl, header);
   }
 
 }
